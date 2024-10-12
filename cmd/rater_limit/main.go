1db5025e94180df2f3fec5065c08ed7af86b7c60
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2024-10-12 00:22:03 -0300

    add: bootstrap application in main

--- cmd/rater_limit/main.go
+++ cmd/rater_limit/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/guirialli/rater_limit/internals/infra/database"
+import (
+	"github.com/guirialli/rater_limit/config"
+	"github.com/guirialli/rater_limit/internals/infra/database"
+	"github.com/guirialli/rater_limit/internals/infra/webserver/router"
+	"github.com/guirialli/rater_limit/internals/infra/webserver/server"
+)
 
 func main() {
 	db, err := database.NewMySql()
@@ -11,6 +16,23 @@ func main() {
 		panic(err)
 	}
 	if err := db.InitDatabase("init.sql"); err != nil {
+		panic(err)
+	}
+
+	con, err := db.GetConnection()
+	if err != nil {
+		panic(err)
+	}
+	cfg, err := config.LoadWebServerConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if err = server.NewServer(cfg, []router.UseRouter{
+		NewBookRouter(con),
+		NewAuthorRouter(con),
+		NewAuthRouter(con),
+	}).Start(); err != nil {
 		panic(err)
 	}
 }
